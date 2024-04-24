e6b9122222dacbb2549f419e679e3f0d865ce771
Tóth Szabolcs Balázs <szabolcs978@gmail.com>
2024-04-24 18:45:20 +0100

    Creating topLine. Some icons and animations.

---
+++ main.js
@@ -0,0 +1,7 @@
+const searchIcon = document.querySelector("#searchIcon");
+const searchBar= document.querySelector("#searchBar");
+
+searchIcon.addEventListener("click", () => {
+    searchBar.focus();
+})
+
