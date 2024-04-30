6ff3496ec3053c1665b6610137fe717cabb453e0
WreckitHUN <szabolcs978@gmail.com>
2024-04-30 15:49:33 +0200

    Remaking the whole project for mobile first approach.

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
-const searchIcon = document.querySelector("#searchIcon");
-const searchBar= document.querySelector("#searchBar");
+const root = document.documentElement;
+root.className = "light";
 
-searchIcon.addEventListener("click", () => {
-    searchBar.focus();
+const setTheme = () => {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+}
+
+const nav = document.querySelector(".nav");
+const arrow = document.querySelector(".arrow");
+
+arrow.addEventListener("click", () => {
+    arrow.classList.toggle("open");
+    nav.classList.toggle("open");
 })
-
+  
+  
